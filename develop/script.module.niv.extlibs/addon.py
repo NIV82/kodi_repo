ba6ea6c231dca0a6d93497150df42e6860eb053c
NIV82 <store.niv@yandex.ru>
2025-11-07 17:42:19 +0300

    add extlib

---
+++ develop/script.module.niv.extlibs/addon.py
@@ -0,0 +1,18 @@
+"""
+Contains:
+    - Name: httpx, Version: 0.28.1, Requires-Python: >=3.8, dependencies (
+        * Name: hyperframe, Version: 6.0.1,     Requires-Python: >=3.6.1;
+        * Name: anyio,      Version: 4.11.0,    Requires-Python: >=3.9;
+        * Name: certifi,    Version: 2025.10.5, Requires-Python: >=3.7;
+        * Name: idna,       Version: 3.11,      Requires-Python: >=3.8;
+        * Name: sniffio,    Version: 1.3.1,     Requires-Python: >=3.7;
+        * Name: h11,        Version: 0.16.0,    Requires-Python: >=3.8;
+        * Name: httpcore,   Version: 1.0.9,     Requires-Python: >=3.8;
+        * Name: h2,         Version: 4.1.0,     Requires-Python: >=3.6.1;
+        * Name: hpack,      Version: 4.0.0,     Requires-Python: >=3.6.1;
+        * Name: socksio,    Version: 1.0.0,     Requires-Python: >=3.6;
+        )
+
+For Kodi 21+ (Omega)
+"""
+from resources.lib import *
